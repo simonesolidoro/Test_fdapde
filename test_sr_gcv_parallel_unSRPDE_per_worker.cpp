b04d59be3442b1272efa426dcc0aa2d7af079c35
simo <simone1.solidoro@mail.polimi.it>
2025-12-06 16:30:33 +0000

    thread_local SRPDE per ogni worker, definizione dentro obj che fa wrap di m.gcv, verificato che thread_local SRPDE creato solo la prima volta che un thread chiama obj poi altre volte ignora e chiama solo m.gcv() su m thread_local. da capire meglio ce funziona, mi sembra di si e sembra scalare bene (timer su optimize()). se funziona prossimo potrebbe essere dividere SRPDE in due classi una con tutte le matrici ecc che vanno create in single thread una volta sola e poi solo lette quindi thread_safe accesso concorrente tramite reference, l'altra con quello che rimane così da creare per ogni thread solo quella

--- test_sr_gcv_parallel_unSRPDE_per_worker.cpp
+++ test_sr_gcv_parallel_unSRPDE_per_worker.cpp
@@ -19,15 +19,24 @@ int main(int argc, char** argv){
     ZeroField<2> u;
     auto F = integral(D)(u * v);
     // modeling
-    SRPDE m("y ~ f", data, fe_ls_elliptic(a, F));
+    //thread_local SRPDE m("y ~ f", data, fe_ls_elliptic(a, F));
     // calibration
-    std::vector<double> lambda_grid(13);
-    for (int i = 0; i < 13; ++i) { lambda_grid[i] = std::pow(10, -6.0 + 0.25 * i) / data[0].rows(); }
+    std::vector<double> lambda_grid(130);
+    for (int i = 0; i < 130; ++i) { lambda_grid[i] = std::pow(10, -6.0 + 0.25 * i) / data[0].rows(); }
     GridSearch<1> optimizer;
     //creo theradpool
     threadpool<steal::random> Tp(1000,n_worker);
+    auto obj = [&](Eigen::Matrix<double, 1, 1> lambda){
+        thread_local SRPDE m("y ~ f", data, fe_ls_elliptic(a, F));//credo che la costruisce ogni thread la prima volta e poi ignorato se già costruito. Si messo cout in costruttore con id thread e ogni thread lo costruisce una volta sola. è orribile e sicuramente non corretto ma sembra funzionare
+        return m.gcv(100, 476813).operator()(lambda);};
 
-    optimizer.optimize(m.gcv(100, 476813), lambda_grid, execution::par,Tp,granularity);
+    auto start = std::chrono::high_resolution_clock::now();
+    optimizer.optimize(obj, lambda_grid, execution::par,Tp,granularity);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);  
+    std::cout<<duration.count()<<" ";
+    //optimizer.optimize(m.gcv(100, 476813), lambda_grid, execution::par,Tp,granularity);
+    
 
     std::cout<<"ottimo"<<optimizer.optimum()<<"value:"<<optimizer.value();
     for (auto&  i : optimizer.values()){
