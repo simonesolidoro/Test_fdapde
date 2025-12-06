ff4f761b0b88281b4607ca7d38ca4658eeee3ee7
simo <simone1.solidoro@mail.polimi.it>
2025-12-06 18:47:31 +0000

    versione parallel per sr spazio tempo separabili. sempre thread_local tutto SRPDE, sembra funzionare.

--- test_sr_spaziotemposeparabile_gcv.cpp
+++ test_sr_spaziotemposeparabile_gcv.cpp
@@ -45,7 +45,7 @@ int main(int argc, char** argv){
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);  
     std::cout<<duration.count()<<" ";
-
+    std::cout<<"ottimo"<<optimizer.optimum()<<"value:"<<optimizer.value();
 
 
     // verifica stessa soluzione 
